695b39d8d54a5285f479af7e447b372bad8e8a06
LittleStar <59785146+LinZhihao-723@users.noreply.github.com>
2023-05-24 14:15:00 -0400

    Replace high_resolution_clock by the system clock, and test wheel on macos.

--- src/clp_ffi/clp_loglib_native/encoding_method.cpp
+++ src/clp_ffi/clp_loglib_native/encoding_method.cpp
@@ -10,7 +10,7 @@ namespace clp_loglib_native::encoder {
         const char* input_timestamp_format;
         const char* input_timezone;
 
-        if (!PyArg_ParseTuple(args, "Lss", &ref_timestamp, &input_timestamp_format,
+        if (false == PyArg_ParseTuple(args, "Lss", &ref_timestamp, &input_timestamp_format,
             &input_timezone))
         {
             PyErr_SetString(PyExc_RuntimeError,
@@ -22,7 +22,7 @@ namespace clp_loglib_native::encoder {
         std::string timezone(input_timezone);
         static const std::string timestamp_pattern_syntax = "";
         static std::vector<int8_t> ir_buf;
-        if (!ffi::ir_stream::four_byte_encoding::encode_preamble(timestamp_format,
+        if (false == ffi::ir_stream::four_byte_encoding::encode_preamble(timestamp_format,
             timestamp_pattern_syntax, timezone, ref_timestamp, ir_buf))
         {
             PyErr_SetString(PyExc_RuntimeError,
@@ -42,7 +42,7 @@ namespace clp_loglib_native::encoder {
     PyObject* encode_message_and_timestamp (PyObject* self, PyObject* args) {
         ffi::epoch_time_ms_t ref_timestamp;
         Py_buffer input_buffer;
-        if (!PyArg_ParseTuple(args, "Ly*", &ref_timestamp, &input_buffer)) {
+        if (false == PyArg_ParseTuple(args, "Ly*", &ref_timestamp, &input_buffer)) {
             PyErr_SetString(PyExc_RuntimeError, "Native encoder failed to parse Python arguments.");
             Py_RETURN_NONE;
         }
@@ -52,13 +52,14 @@ namespace clp_loglib_native::encoder {
 
         std::string_view msg(static_cast<const char*>(input_buffer.buf), input_buffer.len);
         const auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(
-            std::chrono::high_resolution_clock::now().time_since_epoch()
+            std::chrono::system_clock::now().time_since_epoch()
         ).count();
         const ffi::epoch_time_ms_t current_timestamp = static_cast<ffi::epoch_time_ms_t>(ms);
         const auto delta = current_timestamp - ref_timestamp;
 
         // Encode the message
-        if (!ffi::ir_stream::four_byte_encoding::encode_message(delta, msg, logtype, ir_buf)) {
+        if (false == ffi::ir_stream::four_byte_encoding::encode_message(delta, msg, logtype, 
+            ir_buf)) {
             PyBuffer_Release(&input_buffer);
             PyErr_SetString(PyExc_RuntimeError,
                 "Native message encoder failed to encode the message.");
@@ -80,7 +81,7 @@ namespace clp_loglib_native::encoder {
 
     PyObject* encode_message (PyObject* self, PyObject* args) {
         Py_buffer input_buffer;
-        if (!PyArg_ParseTuple(args, "y*", &input_buffer)) {
+        if (false == PyArg_ParseTuple(args, "y*", &input_buffer)) {
             PyErr_SetString(PyExc_RuntimeError, "Native encoder failed to parse Python arguments.");
             Py_RETURN_NONE;
         }
@@ -89,7 +90,7 @@ namespace clp_loglib_native::encoder {
         std::vector<int8_t> ir_buf;
 
         std::string_view msg(static_cast<const char*>(input_buffer.buf), input_buffer.len);
-        if (!ffi::ir_stream::four_byte_encoding::encode_message(msg, logtype, ir_buf)) {
+        if (false == ffi::ir_stream::four_byte_encoding::encode_message(msg, logtype, ir_buf)) {
             PyErr_SetString(PyExc_RuntimeError, "Native encoder failed to encode the message.");
             Py_RETURN_NONE;
         }
@@ -105,19 +106,19 @@ namespace clp_loglib_native::encoder {
 
     PyObject* encode_timestamp (PyObject* self, PyObject* args) {
         ffi::epoch_time_ms_t ref_timestamp;
-        if (!PyArg_ParseTuple(args, "L", &ref_timestamp)) {
+        if (false == PyArg_ParseTuple(args, "L", &ref_timestamp)) {
             PyErr_SetString(PyExc_RuntimeError, "Native encoder failed to parse Python arguments.");
             Py_RETURN_NONE;
         }
 
         const auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(
-            std::chrono::high_resolution_clock::now().time_since_epoch()
+            std::chrono::system_clock::now().time_since_epoch()
         ).count();
         const ffi::epoch_time_ms_t current_timestamp = static_cast<ffi::epoch_time_ms_t>(ms);
         const auto delta = current_timestamp - ref_timestamp;
 
         std::vector<int8_t> ir_buf;
-        if (!ffi::ir_stream::four_byte_encoding::encode_timestamp(delta, ir_buf)) {
+        if (false == ffi::ir_stream::four_byte_encoding::encode_timestamp(delta, ir_buf)) {
             PyErr_SetString(PyExc_RuntimeError,
                 "Native encoder failed to encode the timestamp delta.");
             Py_RETURN_NONE;
